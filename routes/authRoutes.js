ec467f48cc267d3a87e76a0c8db4c8810391cd20
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-07-18 18:13:10 +0200

    disables: register router

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 import authenticateUser from '../middleware/auth.js';
 
-router.route('/register').post(register);
+// router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 
